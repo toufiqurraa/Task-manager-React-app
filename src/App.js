15e73bf3c6a123a3a3f4a096310594d52e705b66
Toufiqur Rahman <60101798+IAmToufiqurRahman@users.noreply.github.com>
2021-05-10 01:20:47 +0600

    Upto Delete Task

--- src/App.js
+++ src/App.js
@@ -25,10 +25,20 @@ export default function App() {
       reminder: false
     }
   ]);
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No Task Availabe"
+      )}
     </div>
   );
 }
